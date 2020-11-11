d609f9ddb918f154b4dacfbe74ba432a66653eec
Rehoboth Okorie <okorie.23@dartmouth.edu>
2020-11-10 23:26:31 -0500

    generating_categories

--- yhacks-app/src/routes/index.js
+++ yhacks-app/src/routes/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route, Redirect } from 'react-router';
+import { Switch, Route} from 'react-router';
 import { BrowserRouter as Router } from "react-router-dom"
 import { connect } from "react-redux"
 import LandingScreen from '../screens/landingScreen';
